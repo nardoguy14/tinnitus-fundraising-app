7878affb940a7b8910a4f7ae26ad03ea952d0ba2
nardoguy14 <nardoarevalo@me.com>
2021-02-21 11:57:38 -0800

    Making actual class component

--- src/InfoComponent/InfoComponent.js
+++ src/InfoComponent/InfoComponent.js
@@ -5,8 +5,15 @@ import bootstrap from 'bootstrap';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
-function InfoComponent() {
-  return (
+class InfoComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
         <div className="jdrf-p2p-greeting__content-tabs js--event-page-tabs" aria-hidden="false">
           <Tabs className="jdrf-p2p-tabs ">
             <TabList style={{transform: "translate(0px, 0px)"}}>
@@ -17,7 +24,6 @@ function InfoComponent() {
                 <span>Walk News</span>
               </Tab>
             </TabList>
-
             <TabPanel >
               <div>
                 <div className="spirit-long-form-text">
@@ -56,55 +62,55 @@ function InfoComponent() {
                             <div><span>During the afternoon, we will all come together for the One Walk National Ceremony broadcast, where JDRF will highlight all the stories about why we walk, how far we have come and celebrate the unstoppable power of the JDRF ONE Walk Community.</span>
                             </div>
                             <br/>
-                              <h3>JDRF One Walk Strava Integration</h3>
-                              <div>Strava, a social-fitness network that tracks cycling, running and walking
-                                activities, is now integrated with your Participant Center so you can see the distance
-                                we will walk together in pursuit of our collective goal of walking 1.6 million miles
-                                by November 1st.
-                              </div>
-                              <div>
-                                <p>&nbsp;</p>
-                              </div>
-                              <h3>2020 One Walk t-shirt&nbsp;</h3>
-                              <p>Walkers that fundraise $100 for JDRF will be mailed the 2020 shirt.</p>
-                              <p></p>
-                              <h3>Walk in a Box&nbsp;</h3>
-                              <div>Teams that raise $5,000 will receive a JDRF One Walk In A Box before November
-                                1st.
-                              </div>
-                              <div>&nbsp;</div>
-                              <div>&nbsp;</div>
-                              <h3>Mile of Hope</h3>
-                              <div>The Mile of Hope is a special way to recognize all our One Walk teams that have
-                                raised $100+ by October 12, 2020. This exclusive group includes family, school and
-                                coporate teams. Teams reaching the Mile of Hope will be recognized during the Finish
-                                Line Festival.
-                              </div>
-                              <div>&nbsp;</div>
-                              <div>&nbsp;</div>
-                              <h3>VIP&nbsp;</h3>
-                              <p>V1P status is exclusively awarded to JDRF One Walk fundraisers who individually raise
-                                $1,000 or more. To celebrate your dedication to raising money for T1D research, you'll
-                                receive a special 2020 One Walk V1P t-shirt, a V1P lanyard and badge and V1P award.
-                                V1Ps will also be recognized during the Finish Line Festival on November 1.&nbsp;<span
-                                    style= {{'font-size': '10pt'}}>&nbsp;</span></p>
-                              <h3>T-Shirt Gallery</h3>
-                              <div>Teams will have the opportunity to display their team t-shirts n the T-shirt
-                                Gallery. Here you can display your favorite t-shirt from the past or the one this
-                                year.
-                              </div>
-                              <div>&nbsp;</div>
-                              <h3>In-Game Experience with Roblox</h3>
-                              <div>Experience the first-ever JDRF gaming collaboration with Roblox! Explore the One
-                                Walk site through the gaming environment with your team members and friends, just as
-                                you would at your local One Walk.
-                              </div>
-                              <div>&nbsp;</div>
-                              <h3>Partner Pavilion</h3>
-                              <div>Meet with local and national vendors in the Partner Pavilion. Participants will
-                                have the opportunity to connect virtually with vendors to ask question and learn about
-                                new products.
-                              </div>
+                            <h3>JDRF One Walk Strava Integration</h3>
+                            <div>Strava, a social-fitness network that tracks cycling, running and walking
+                              activities, is now integrated with your Participant Center so you can see the distance
+                              we will walk together in pursuit of our collective goal of walking 1.6 million miles
+                              by November 1st.
+                            </div>
+                            <div>
+                              <p>&nbsp;</p>
+                            </div>
+                            <h3>2020 One Walk t-shirt&nbsp;</h3>
+                            <p>Walkers that fundraise $100 for JDRF will be mailed the 2020 shirt.</p>
+                            <p></p>
+                            <h3>Walk in a Box&nbsp;</h3>
+                            <div>Teams that raise $5,000 will receive a JDRF One Walk In A Box before November
+                              1st.
+                            </div>
+                            <div>&nbsp;</div>
+                            <div>&nbsp;</div>
+                            <h3>Mile of Hope</h3>
+                            <div>The Mile of Hope is a special way to recognize all our One Walk teams that have
+                              raised $100+ by October 12, 2020. This exclusive group includes family, school and
+                              coporate teams. Teams reaching the Mile of Hope will be recognized during the Finish
+                              Line Festival.
+                            </div>
+                            <div>&nbsp;</div>
+                            <div>&nbsp;</div>
+                            <h3>VIP&nbsp;</h3>
+                            <p>V1P status is exclusively awarded to JDRF One Walk fundraisers who individually raise
+                              $1,000 or more. To celebrate your dedication to raising money for T1D research, you'll
+                              receive a special 2020 One Walk V1P t-shirt, a V1P lanyard and badge and V1P award.
+                              V1Ps will also be recognized during the Finish Line Festival on November 1.&nbsp;<span
+                                  style= {{'font-size': '10pt'}}>&nbsp;</span></p>
+                            <h3>T-Shirt Gallery</h3>
+                            <div>Teams will have the opportunity to display their team t-shirts n the T-shirt
+                              Gallery. Here you can display your favorite t-shirt from the past or the one this
+                              year.
+                            </div>
+                            <div>&nbsp;</div>
+                            <h3>In-Game Experience with Roblox</h3>
+                            <div>Experience the first-ever JDRF gaming collaboration with Roblox! Explore the One
+                              Walk site through the gaming environment with your team members and friends, just as
+                              you would at your local One Walk.
+                            </div>
+                            <div>&nbsp;</div>
+                            <h3>Partner Pavilion</h3>
+                            <div>Meet with local and national vendors in the Partner Pavilion. Participants will
+                              have the opportunity to connect virtually with vendors to ask question and learn about
+                              new products.
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -128,7 +134,8 @@ function InfoComponent() {
             </TabPanel>
           </Tabs>
         </div>
-  );
+    );
+  }
 }
 
 export default InfoComponent;
