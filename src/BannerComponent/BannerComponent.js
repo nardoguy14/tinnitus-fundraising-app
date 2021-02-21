0b05fe82b7d629affb2aabd23e606bd2d17bdd8f
nardoguy14 <nardoarevalo@me.com>
2021-02-21 15:37:18 -0800

    Moving banner info to state instead of html

--- src/BannerComponent/BannerComponent.js
+++ src/BannerComponent/BannerComponent.js
@@ -3,29 +3,41 @@ import ReactDOM from 'react-dom';
 import './BannerComponent.css';
 import bootstrap from 'bootstrap';
 
-function BannerComponent() {
-  return (
+class BannerComponent extends React.Component  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundUrl: "https://www2.jdrf.org/jdrf-framework/dist/walk/images/greeting-page-cover/default.compressed.jpg",
+      eventTitle: "JDRF One Walk, Los Angeles 2020",
+      eventDate: "November 1, 2020",
+      eventLocation: "Los Angeles, CA"
+    }
+  }
 
+  render() {
+    let {backgroundUrl, eventTitle, eventDate, eventLocation} = this.state
+    return (
       <div className="jdrf-p2p-cover-photo-wrap jdrf-p2p-cover-photo-wrap--wide">
-        <div className="jdrf-p2p-cover-photo ng-isolate-scope">
-          <div className="jdrf-p2p-cover-photo__inner"
-               style={{'background-image': 'url("https://www2.jdrf.org/jdrf-framework/dist/walk/images/greeting-page-cover/default.compressed.jpg")'}}>
-            <div className="jdrf-p2p-cover-photo__meta hidden-spirit-xs hidden-spirit-sm">
-              <div className="jdrf-p2p-cover-photo__text">
-                <h1 className="spirit-h2">
-                  <span>JDRF One Walk, Los Angeles 2020</span>
-                </h1>
-                <div className="jdrf-p2p-cover-photo__subheading">
-                  <span>November 1, 2020</span>
-                  <span>&nbsp;•&nbsp;</span>
-                  <span>Los Angeles, CA</span>
+          <div className="jdrf-p2p-cover-photo ng-isolate-scope">
+            <div className="jdrf-p2p-cover-photo__inner"
+                 style={{'background-image': 'url(' + backgroundUrl + ')'}}>
+              <div className="jdrf-p2p-cover-photo__meta hidden-spirit-xs hidden-spirit-sm">
+                <div className="jdrf-p2p-cover-photo__text">
+                  <h1 className="spirit-h2">
+                    <span>{eventTitle}</span>
+                  </h1>
+                  <div className="jdrf-p2p-cover-photo__subheading">
+                    <span>{eventDate}</span>
+                    <span>&nbsp;•&nbsp;</span>
+                    <span>{eventLocation}</span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-  );
+    );
+  }
 }
 
 export default BannerComponent;
