b0e781e6eb69cf4f1fc679682f6ed546e3359da8
nardoguy14 <nardoarevalo@me.com>
2021-02-21 14:00:23 -0800

    Pulling data out of distance component into state

--- src/DistanceElapsedComponent/DistanceElapsedComponent.js
+++ src/DistanceElapsedComponent/DistanceElapsedComponent.js
@@ -3,28 +3,38 @@ import ReactDOM from 'react-dom';
 import './DistanceElapsedComponent.css';
 import bootstrap from 'bootstrap';
 
-function DistanceElapsedComponent() {
-  return (
-      <div className="jdrf-p2p-greeting__strava-activities">
-        <h2 className="spirit-h3">Our Miles Towards Mission</h2>
-        <p className="spirit-body-text-l">
-          In support of the 1.6 million Americans living with T1D, our miles will be
-          counted towards a collective goal of walking 1.6 million
-          miles.
-        </p>
-        <div className="jdrf-p2p-progress-meter-wrap jdrf-p2p-progress-meter-wrap--small js--fitness-progress-meter-wrap ng-scope">
-          <div className="jdrf-p2p-progress-meter ng-isolate-scope jdrf-p2p-progress-meter--no-goal">
-            <div className="clearfix">
-              <div className="jdrf-p2p-progress-meter__achieved clearfix">
-                <div className="jdrf-p2p-progress-meter__amount ng-binding ng-scope">
-                    2,739.05 mi
+class DistanceElapsedComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: "In support of the 1.6 million Americans living with T1D, our miles will be counted towards " +
+                         "a collective goal of walking 1.6 million miles.",
+            totalDistance: "2,739.05 mi"
+        }
+    }
+
+    render() {
+        let {totalDistance, description} = this.state
+        return (
+            <div className="jdrf-p2p-greeting__strava-activities">
+                <h2 className="spirit-h3">Our Miles Towards Mission</h2>
+                <p className="spirit-body-text-l">
+                    {description}
+                </p>
+                <div className="jdrf-p2p-progress-meter-wrap jdrf-p2p-progress-meter-wrap--small js--fitness-progress-meter-wrap ng-scope">
+                    <div className="jdrf-p2p-progress-meter ng-isolate-scope jdrf-p2p-progress-meter--no-goal">
+                        <div className="clearfix">
+                            <div className="jdrf-p2p-progress-meter__achieved clearfix">
+                                <div className="jdrf-p2p-progress-meter__amount ng-binding ng-scope">
+                                    {totalDistance}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-  );
+        )
+    }
 }
 
 export default DistanceElapsedComponent;
