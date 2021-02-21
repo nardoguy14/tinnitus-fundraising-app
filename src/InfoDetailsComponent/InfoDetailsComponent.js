31d5cd8f449847055d7fa90131e79b473e504d2d
nardoguy14 <nardoarevalo@me.com>
2021-02-21 15:24:40 -0800

    Pulling details out into state

--- src/InfoDetailsComponent/InfoDetailsComponent.js
+++ src/InfoDetailsComponent/InfoDetailsComponent.js
@@ -3,78 +3,76 @@ import ReactDOM from 'react-dom';
 import './InfoDetailsComponent.css';
 import bootstrap from 'bootstrap';
 
-function InfoDetailsComponent() {
-  return (
+class InfoDetailsComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: "November 1, 2020",
+      startTime: "6 AM PST",
+      site: "https://jdrfonewalk.virtualeventsite.com/southernca",
+      contactPerson: "Sue Pietrzak",
+      contactEmail: "abc@me.com",
+      contactPhone: "(760) 791-3504",
+      teamName: "Southern California Chapter"
+    }
+  }
 
-    <div>
-      <div className="jdrf-p2p-page-details">
-        <div className="spirit-list">
-          <div className="spirit-list-item spirit-list-item--avatar">
-            <div className="spirit-list-item__avatar">
-              <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
-            </div>
-            <div className="spirit-list-item__content">
-              <div className="spirit-list-item__title">
-                <span className="ng-binding">November 1, 2020</span>
+  render() {
+    let {startDate, startTime, site, contactPerson, contactEmail, contactPhone, teamName} = this.state
+    return (
+        <div>
+          <div className="jdrf-p2p-page-details">
+            <div className="spirit-list">
+              <div className="spirit-list-item spirit-list-item--avatar">
+                <div className="spirit-list-item__avatar">
+                  <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
+                </div>
+                <div className="spirit-list-item__content">
+                  <div className="spirit-list-item__title">
+                    <span className="ng-binding">{startDate}</span>
+                  </div>
+                  <div className="spirit-list-item__meta">
+                    <div> Starts: {startTime}</div>
+                  </div>
+                </div>
               </div>
-              <div className="spirit-list-item__meta">
-                <div> Starts: 6 AM PST</div>
-              </div>
-            </div>
-          </div>
 
-          <div className="spirit-list-item spirit-list-item--avatar">
-            <div className="spirit-list-item__avatar">
-              <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
-            </div>
-            <div className="spirit-list-item__content">
-              <div className="spirit-list-item__title">
-                https://jdrfonewalk.virtualeventsite.com/southernca
+              <div className="spirit-list-item spirit-list-item--avatar">
+                <div className="spirit-list-item__avatar">
+                  <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
+                </div>
+                <div className="spirit-list-item__content">
+                  <div className="spirit-list-item__title">
+                    {site}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div className="spirit-list-item spirit-list-item--avatar">
-            <div className="spirit-list-item__avatar">
-              <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
-            </div>
-            <div className="spirit-list-item__content">
-              <div className="spirit-list-item__title"> Sue Pietrzak</div>
-              <div className="spirit-list-item__links-contain">
-                <a className="spirit-list-item__link" href="mailto:spietrzak@jdrf.org">Email</a>&nbsp;| (213) 769-6320
+              <div className="spirit-list-item spirit-list-item--avatar">
+                <div className="spirit-list-item__avatar">
+                  <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
+                </div>
+                <div className="spirit-list-item__content">
+                  <div className="spirit-list-item__title"> {contactPerson}</div>
+                  <div className="spirit-list-item__links-contain">
+                    <a className="spirit-list-item__link" href={"mailto:" + contactEmail} >Email</a>&nbsp;| {contactPhone}
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
 
-          <div className="spirit-list-item spirit-list-item--avatar">
-            <div className="spirit-list-item__avatar">
-              <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
-            </div>
-            <div className="spirit-list-item__content">
-              <div className="spirit-list-item__title">Southern California Chapter</div>
-              <div className="spirit-list-item__meta">
-                Mail Checks To: JDRF<br/>
-                3001 Metro Drive Suite 100<br/>
-                Bloomington, MN 55425<br/>
+              <div className="spirit-list-item spirit-list-item--avatar">
+                <div className="spirit-list-item__avatar">
+                  <svg className="spirit-icon spirit-list-item__icon" aria-hidden="true"></svg>
+                </div>
+                <div className="spirit-list-item__content">
+                  <div className="spirit-list-item__title">{teamName}</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="jdrf-p2p-greeting__secondary-cta">
-        <div className="spirit-row">
-          <div
-              className="spirit-col-12">
-            <button type="button"
-                    className="spirit-button spirit-button--secondary spirit-button--with-icon spirit-button--fullwidth ng-scope">
-              Share
-              <svg className="spirit-icon spirit-icon--share spirit-icon--share-right" aria-hidden="true"></svg>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default InfoDetailsComponent;
